dc8c3a25a1c89a5f651f4f280fb77be78cb1fdfa
C97-BADREDDINE <bakayombo@gmail.com>
2023-12-27 19:55:28 +0100

    button_v1

--- main.c
+++ main.c
@@ -1,16 +1,45 @@
 #include "raylib.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
 
 #define MAX_INPUT_CHARS 9
+#define buttonWidth  180
+#define buttonHeight  70
+
+Vector2 buttonPosition = {100, 100};
+
+void DrawButton(Rectangle rect, const char *text,Color co)
+{
+    DrawRectangleLinesEx(rect, 3, BLANK);
+    DrawRectangleRec(rect, co);                                                                                 // Draw button outline
+    DrawText(text, rect.x + (rect.width - MeasureText(text, 20)) / 2, rect.y + (rect.height - 20) / 2, 21, WHITE); // Draw button text
+}
+
+bool isClicked(Rectangle rec)
+{
+    if (CheckCollisionPointRec(GetMousePosition(), rec) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "My Full-Screen Interface");
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "Raylib Demo");
+    
 
-    char name[MAX_INPUT_CHARS + 1] = "\0"; // Buffer to store input text (plus null terminator)
-    int letterCount = 0;                   // Current input text length
+
+
+    Rectangle buttonCreate = {buttonPosition.x, buttonPosition.y, buttonWidth, buttonHeight};
+    Rectangle buttoninsert = {buttonPosition.x, buttonPosition.y + buttonHeight + 10, buttonWidth, buttonHeight};
+    Rectangle buttonRecherche = {buttonPosition.x, buttonPosition.y + 2 * (buttonHeight + 10), buttonWidth, buttonHeight};
+    Rectangle buttonDelete = {buttonPosition.x, buttonPosition.y + 3 * (buttonHeight + 10), buttonWidth, buttonHeight};
 
     SetTargetFPS(60);
 
@@ -21,43 +50,21 @@ int main(void)
         { // Toggle fullscreen on F11
             ToggleFullscreen();
         }
-        // Check for input to add characters to the text
-        if (letterCount < MAX_INPUT_CHARS)
-        {
-            int key = GetKeyPressed();
-
-            if (key != 0)
-            {
-                // Only add ASCII characters
-                if ((key >= 32) && (key <= 125))
-                {
-                    name[letterCount] = (char)key;
-                    letterCount++;
-                }
-            }
-        }
-
-        // Check for backspace key to remove characters from the text
-        if (IsKeyPressed(KEY_BACKSPACE))
-        {
-            if (letterCount > 0)
-            {
-                letterCount--;
-                name[letterCount] = '\0';
-            }
-        }
 
         // Draw
-
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+
+        ClearBackground(BLACK);
 
         // Draw text input rectangle
-        DrawRectangle(screenWidth / 2 - 150, screenHeight / 2 - 30, 300, 60, LIGHTGRAY);
-        DrawRectangleLines(screenWidth / 2 - 150, screenHeight / 2 - 30, 300, 60, GRAY);
+        DrawButton(buttonCreate, "Create",GREEN);
+        DrawButton(buttoninsert, "Insert",YELLOW);
+        DrawButton(buttonRecherche, "Rechercher",ORANGE);
+        DrawButton(buttonDelete, "Delete",RED);
 
-        // Draw text
-        DrawText(name, screenWidth / 2 - MeasureText(name, 20) / 2, screenHeight / 2 - 10, 20, BLACK);
+        if(isClicked(buttoninsert)){
+            DrawRectangle(900,300,buttonWidth,buttonHeight,VIOLET);
+        }
 
         EndDrawing();
     }
