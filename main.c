3b14192f5c781593324035d4896baad31c24d5b4
Aymen <aymen.boukaoula@etu.usthb.dz>
2023-12-28 16:57:43 +0100

    creation_liste

--- main.c
+++ main.c
@@ -1,215 +1,101 @@
-#include "raylib.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "stdbool.h"
+#include "include/raylib.h"
 
-typedef struct Node
-{
+#include <raylib.h>
+
+typedef struct Node {
     int data;
     struct Node *prev;
     struct Node *next;
 } Node;
 
-Node *createNode(int data)
-{
+Node *head = NULL;
+
+void insertNode(int data) {
     Node *newNode = (Node *)malloc(sizeof(Node));
     newNode->data = data;
-    newNode->next = NULL;
     newNode->prev = NULL;
-    return newNode;
+    newNode->next = head;
+
+    if (head != NULL) {
+        head->prev = newNode;
+    }
+
+    head = newNode;
 }
 
-void insertNode(Node **head, int data)
-{
-    Node *newNode = createNode(data);
-    newNode->next = *head;
-    if (*head != NULL)
-    {
-        (*head)->prev = newNode;
-    }
-    *head = newNode;
-}
-void deleteNode(Node **head, int data)
-{
-    Node *current = *head;
-    while (current != NULL && current->data != data)
-    {
-        current = current->next;
-    }
-    if (current == NULL)
-    {
-        printf("No such node found\n");
-        return;
-    }
-
-    if (current->prev != NULL)
-    {
-        current->prev->next = current->next;
-    }
-    else
-    {
-        *head = current->next;
-    }
-
-    if (current->next != NULL)
-    {
-        current->next->prev = current->prev;
-    }
-
-    free(current);
-}
-
-Node *searchNode(Node *head, int data)
-{
-    while (head != NULL && head->data != data)
-    {
+void deleteNode() {
+    if (head != NULL) {
+        Node *temp = head;
         head = head->next;
-    }
-    return head;
-}
-// Function to print the doubly linked list
-void display(Node *head)
-{
-    while (head != NULL)
-    {
-        printf("%d ", head->data);
-        head = head->next;
-    }
-}
 
-void SwapNodes(Node **head, Node *node1, Node *node2)
-{
-    // Vérifier si les noeuds sont les memes
-    if (node1 == node2)
-        return;
-
-    // Vérifier si l'un des noeuds est NULL
-    if (node1 == NULL || node2 == NULL)
-        return;
-
-    // Trouver les noeuds voisins
-    Node *prevNode1 = node1->prev;
-    Node *nextNode1 = node1->next;
-    Node *prevNode2 = node2->prev;
-    Node *nextNode2 = node2->next;
-
-    // Mettre à jour les liens pour le noeud 1
-    if (prevNode1 != NULL)
-    {
-        prevNode1->next = node2;
-    }
-    else
-    {
-        *head = node2; // Si le nœud 1 est la tête de la liste
-    }
-
-    node2->prev = prevNode1;
-    node2->next = nextNode1;
-
-    // Mettre à jour les liens pour le nœud 2
-    if (prevNode2 != NULL)
-    {
-        prevNode2->next = node1;
-    }
-    else
-    {
-        *head = node1; // Si le nœud 2 est la tête de la liste
-    }
-
-    node1->prev = prevNode2;
-    node1->next = nextNode2;
-}
-
-void TriListDoublement(Node **head)
-{
-    if (*head == NULL || (*head)->next == NULL)
-    {
-        return;
-    }
-    Node *current = *head;
-    Node *suiv;
-    while (current != NULL)
-    {
-        suiv = current->next;
-        while (suiv != NULL)
-        {
-            if (current->data > suiv->data)
-            {
-                SwapNodes(head, current, suiv);
-            }
-            else
-            {
-                suiv = suiv->next;
-            }
+        if (head != NULL) {
+            head->prev = NULL;
         }
+
+        free(temp);
     }
 }
 
-#define MAX_INPUT_CHARS 9
-#define buttonWidth 180
-#define buttonHeight 70
+void drawList() {
+    Node *current = head;
+    int xPos = 10;
+    int rectWidth = 20;
+    int rectHeight = 20;
+    int arrowSpacing = 20; // Ajustez cette valeur pour séparer davantage les flèches
 
-Vector2 buttonPosition = {100, 100};
+    while (current != NULL) {
+        // Dessiner le rectangle avec la valeur
+        DrawRectangle(xPos, 50, rectWidth, rectHeight, LIGHTGRAY);
+        DrawText(TextFormat("%d", current->data), xPos + 5, 55, 10, BLACK);
 
-void DrawButton(Rectangle rect, const char *text, Color co)
-{
-    DrawRectangleLinesEx(rect, 3, BLANK);
-    DrawRectangleRec(rect, co);                                                                                    // Draw button outline
-    DrawText(text, rect.x + (rect.width - MeasureText(text, 20)) / 2, rect.y + (rect.height - 20) / 2, 21, WHITE); // Draw button text
+        // Dessiner la flèche suivante (si elle existe)
+        if (current->next != NULL) {
+            DrawLine(xPos + rectWidth, 60 - arrowSpacing, xPos + rectWidth + 20, 60 - arrowSpacing, BLACK);  // ligne horizontale
+            DrawLine(xPos + rectWidth + 20, 60 - arrowSpacing, xPos + rectWidth + 15, 55 - arrowSpacing, BLACK);  // flèche vers le haut
+            DrawLine(xPos + rectWidth + 20, 60 - arrowSpacing, xPos + rectWidth + 15, 65 - arrowSpacing, BLACK);  // flèche vers le bas
+        }
+
+        // Dessiner la flèche précédente (si elle existe)
+        if (current->prev != NULL) {
+            DrawLine(xPos, 60 + arrowSpacing, xPos - 20, 60 + arrowSpacing, BLACK);  // ligne horizontale
+            DrawLine(xPos - 20, 60 + arrowSpacing, xPos - 15, 55 + arrowSpacing, BLACK);  // flèche vers le haut
+            DrawLine(xPos - 20, 60 + arrowSpacing, xPos - 15, 65 + arrowSpacing, BLACK);  // flèche vers le bas
+        }
+
+        current = current->next;
+        xPos += 50;
+    }
 }
 
-bool isClicked(Rectangle rec)
-{
-    if (CheckCollisionPointRec(GetMousePosition(), rec) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-int main(void)
-{
-
-    InitWindow(GetScreenWidth(), GetScreenHeight(), "Raylib Demo");
-
-    Rectangle buttonCreate = {buttonPosition.x, buttonPosition.y, buttonWidth, buttonHeight};
-    Rectangle buttoninsert = {buttonPosition.x, buttonPosition.y + buttonHeight + 10, buttonWidth, buttonHeight};
-    Rectangle buttonRecherche = {buttonPosition.x, buttonPosition.y + 2 * (buttonHeight + 10), buttonWidth, buttonHeight};
-    Rectangle buttonDelete = {buttonPosition.x, buttonPosition.y + 3 * (buttonHeight + 10), buttonWidth, buttonHeight};
+    InitWindow(screenWidth, screenHeight, "Raylib Double Linked List");
 
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
-        // Update
-        if (IsKeyPressed(KEY_F11))
-        { // Toggle fullscreen on F11
-            ToggleFullscreen();
-        }
-
-        // Draw
+    while (!WindowShouldClose()) {
         BeginDrawing();
+        ClearBackground(RAYWHITE);
 
-        ClearBackground(BLACK);
-
-        // Draw text input rectangle
-        DrawButton(buttonCreate, "Create", GREEN);
-        DrawButton(buttoninsert, "Insert", GOLD);
-        DrawButton(buttonRecherche, "Rechercher", ORANGE);
-        DrawButton(buttonDelete, "Delete", RED);
-
-        if (isClicked(buttoninsert))
-        {
-            DrawRectangle(900, 300, buttonWidth, buttonHeight, VIOLET);
+        if (IsKeyPressed(KEY_SPACE)) {
+            int data = GetRandomValue(1, 100);
+            insertNode(data);
         }
+
+        if (IsKeyPressed(KEY_D)) {
+            deleteNode();
+        }
+
+        drawList();
 
         EndDrawing();
     }
-// close fenètre
+
     CloseWindow();
 
     return 0;
-}
